3560b1d7db22347f19d383c69d63229faab967d2
xiyini <q2z@tuta.io>
2021-11-06 21:35:27 +0400

    Add UI

--- app.js
+++ app.js
@@ -1,94 +1,115 @@
 // Rock Paper Scissors
 
-// -- run the game
-game();
+// -- Declare var
+const container = document.querySelector(".container");
+const startButtun = document.querySelector("[type='start']");
+const buttun = document.querySelector(".buttons");
+const playMsg = document.querySelector("h4");
+const gameContent = document.querySelector(".content");
+const choices = document.querySelectorAll(".choices div");
+const section = document.querySelector(".section");
+const music = document.querySelector(".music");
 
+// start buttun
+startButtun.addEventListener("click", newGame);
+
+// active choices for playerSelection by clicking
+choices.forEach((choice) => choice.addEventListener("click", select));
+
+// send playerSelection to playRound
+function select() {
+  playerSelection = this.getAttribute("data-text");
+  playRound();
+}
+
+// auto gen computer selcet
+function computerPlay() {
+  let randomGen = Math.round(Math.random() * 2);
+  return randomGen === 0
+    ? "Rock"
+    : randomGen === 1
+    ? "Paper"
+    : randomGen === 2
+    ? "Scissors"
+    : "";
+}
+
+// set a new game
+function newGame() {
+  music.play();
+  gameContent.style.opacity = "1";
+  gameContent.style.height = "auto";
+  // Declare the start points
+  playerPoints = 0;
+  computerPoints = 0;
+  currentRound = 1;
+
+  // change buttun content after starting the game
+  startButtun.textContent = "reset";
+  // if reset buttun pressed remove the score msg
+  section.children[0].tagName != "DIV" ? section.prepend(gameContent) : "";
+  section.children[1].tagName == "H3" ? section.children[1].remove() : "";
+  // remove press start to paly
+  section.removeChild(playMsg);
+}
+
+// Declare an elements to shows scores
+scoresMsg = document.createElement("h3");
+scoresMsg.classList.add("scores");
+
+function playRound() {
+  // run computerPlay to generate selection
+  computerSelection = computerPlay();
+
+  // Declare var
+  ps = playerSelection;
+  cs = computerSelection;
+
+  // satuations
+  if (
+    (ps === "Rock" && cs === "Scissors") ||
+    (ps === "Paper" && cs === "Rock") ||
+    (ps === "Scissors" && cs === "Paper")
+  ) {
+    scoresMsg.innerHTML = `<span class="res">Round ${currentRound} ${ps} Beats ${cs}</span> <span class="ps">Your Score is: ${++playerPoints}</span> <span class="cs">Computer Score is: ${computerPoints}</span>`;
+    buttun.before(scoresMsg);
+  } else if (
+    (ps === "Scissors" && cs === "Rock") ||
+    (ps === "Rock" && cs === "Paper") ||
+    (ps === "Paper" && cs === "Scissors")
+  ) {
+    scoresMsg.innerHTML = `<span class="res">Round ${currentRound} ${cs} Beats ${ps}</span> <span class="ps">Your Score is: ${playerPoints}</span> <span class="cs">Computer Score is: ${++computerPoints}</span>`;
+    buttun.before(scoresMsg);
+  } else {
+    scoresMsg.innerHTML = `<span class="res">Round ${currentRound} ${ps} and ${cs}</span> <span class="tie">tie!</span>`;
+    buttun.before(scoresMsg);
+  }
+
+  // one round played
+  currentRound++;
+
+  // fn to check 5 rounds or not
+  game();
+}
+
+// when reaching 5 rounds print result and then reset the game
 function game() {
-    // 1. Declare the start points
-    playerPoints = 0;
-    computerPoints = 0;
-
-    // 2. run the game 5 times
-    for (i = 1; i <= 5; i++) {
-        // 2.1. Auto generate computer choice
-        function computerPlay() {
-            let randomGen = Math.round(Math.random() * 2);
-            return randomGen === 0
-                ? "rock"
-                : randomGen === 1
-                    ? "paper"
-                    : randomGen === 2
-                        ? "scissors"
-                        : "";
-        }
-
-        // 2.2. Ask user for his choice
-        playerSelection = window.prompt(
-            `round ${i} - What's Your Choise?`,
-            "Rock Or Paper Or Scissors"
-        );
-
-        // 2.3. start processing the choices
-        function playRound() {
-            // 2.3.1. put in computer choice
-            computerSelection = computerPlay();
-
-            // 2.3.2. put in player choice and compare it with computer choice
-            // and print out result messege
-
-            // 2.3.2.1 if user choose rock ?
-            return playerSelection.toLowerCase() === "rock"
-                ? computerSelection === "rock"
-                    ? `It's a Draw!
-        Your score is: ${++playerPoints}.
-        Computer score is: ${++computerPoints}.`
-                    : computerSelection === "paper"
-                        ? `You Lose! Paper beats Rock
-        Computer score is: ${++computerPoints}`
-                        : `You Win! Rock Beats Scissors
-        Your score is: ${++playerPoints}`
-
-                : // 2.3.2.2. if user choose paper ?
-                playerSelection.toLowerCase() === "paper"
-                    ? computerSelection === "rock"
-                        ? `You Win! Paper Beats Rock
-        Your score is: ${++playerPoints}`
-                        : computerSelection === "paper"
-                            ? `It's a Draw!
-        Your score is: ${++playerPoints}.
-        Computer score is: ${++computerPoints}.`
-                            : `You Lose! Scissors Beats Paper
-        Computer score is: ${++computerPoints}`
-
-                    : // 2.3.2.3. if user choose scissors ?
-                    playerSelection.toLowerCase() === "scissors"
-                        ? computerSelection === "rock"
-                            ? `You Lose! Rock Beats Scissors
-        Computer score is: ${++computerPoints}`
-                            : computerSelection === "paper"
-                                ? `You Win! Scissors Beats Paper
-        Your score is: ${++playerPoints}`
-                                : `It's a Draw!
-        Your score is: ${++playerPoints}.
-        Computer score is: ${++computerPoints}.`
-                        : "somthing went wrong";
-        }
-        // 2.3.3. print out the result each time the game run
-        console.log(playRound());
-
-        // 2.3.4. the game reach 5 times, Print out the total result
-        i === 5
-            ? playerPoints > computerPoints
-                ? console.log(`You Are the Winner!
-                Your Score is: ${playerPoints}
-                computer Score is: ${computerPoints}`)
-                : playerPoints < computerPoints
-                    ? console.log(`Computer is the Winner!
-                    Your Score is: ${playerPoints}
-                    computer Score is: ${computerPoints}`)
-                    : console.log(`It's a Draw! No Winner.
-                    Your Score is: ${playerPoints}
-                    computer Score is: ${computerPoints}`)
-            : "";
+  if (currentRound === 6) {
+    if (playerPoints > computerPoints) {
+      scoresMsg.innerHTML = `<span class="final-result">You Are The Winner!</span> <span class="ps">Your Score is: ${playerPoints}</span> <span class="cs">Computer Score is: ${computerPoints}</span>`;
+      gameContent.remove();
+      buttun.before(scoresMsg);
+      startButtun.textContent = "New Game";
+    } else if (playerPoints < computerPoints) {
+      scoresMsg.innerHTML = `<span class="final-result">Computer Won!</span> <span class="ps">Your Score is: ${playerPoints}</span> <span class="cs">Computer Score is: ${computerPoints}</span>`;
+      gameContent.remove();
+      buttun.before(scoresMsg);
+      startButtun.textContent = "New Game";
+    } else {
+      scoresMsg.innerHTML = `<span class="final-result">Tie!</span> <span class="ps">Your Score is: ${playerPoints}</span> <span class="cs">Computer Score is: ${computerPoints}</span>`;
+      gameContent.remove();
+      buttun.before(scoresMsg);
+      startButtun.textContent = "New Game";
     }
+  }
 }
