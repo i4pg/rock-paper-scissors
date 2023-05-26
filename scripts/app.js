e9feebfc1154dd4b29cf1ef153b22b1bb6b07a26
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-26 09:35:07 +0400

    fix: use one gets that return an array

--- scripts/app.js
+++ scripts/app.js
@@ -1,50 +1,16 @@
 const RPS = ['rock', 'paper', 'scissors']
 
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3) // random number between 0~2
+  let index = Math.floor(Math.random() * 3) // random number between 0~2
+  let choice = RPS[index] // 1 => paper 
+
+  return index, choice // [ 1, 'paper' ]
 }
 
 function getPlayerChoice(choice) {
-  return RPS.indexOf(choice)
+  let choice = choice.toLowerCase() // 'rock'
+  let index = RPS.indexOf(choice) // rock => 0
+
+  return index, choice // [ 0, 'rock' ]
 }
 
-function getPlayerString(choice) {
-  return choice.toLowerCase()
-}
-
-function getComputerString(index) {
-  return RPS[index]
-}
-
-function singleRound(computerSelection, playerSelection) {
-  let winner = getWinner(computerSelection, playerSelection)
-
-  switch (winner) {
-    case value:
-
-      break;
-
-    default:
-      break;
-  }
-}
-
-function getWinner(computerSelection, playerSelection) {
-  const SRP = ['scissors', 'rock', 'paper'] // RPS[0] beats SRP[0]
-
-  let computerSelectionInString = RPS[computerSelection]
-  let computerIndexInSRP = SRP.indexOf(computerSelectionInString)
-
-  switch (playerSelection) {
-    case computerSelection: // both are equal (TIE)
-      console.log('TIE')
-      return -1
-    case computerIndexInSRP: // Player beats Computer
-      console.log(`You Win!  beats Rock`)
-      return 1
-    default: // Computer beats Player
-      return 0
-  }
-}
-
-singleRound(getComputerChoice(), getPlayerString('rock'))
