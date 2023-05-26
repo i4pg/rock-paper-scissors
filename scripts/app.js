6069557acb1bddab033456a672431c2b544d9049
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-26 07:57:01 +0400

    feat: add winning logic

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,29 @@ function getPlayerChoise(choise) {
 }
 
 function round(computerSelection, playerSelection) {
-  console.log(computerSelection, playerSelection)
+  winningCombination(computerSelection, playerSelection)
+}
+
+function winningCombination(computerSelection, playerSelection) {
+  const SPR = RPS.toReversed()
+
+  // using indeces for comparing
+  let p = RPS.indexOf(playerSelection)
+  let c = RPS.indexOf(computerSelection)
+
+  switch (p) {
+    case c:
+      console.log('tie')
+      break;
+    case SPR.indexOf(RPS[c]):
+      console.log('win')
+      break;
+
+    default:
+      console.log('lose')
+      break;
+  }
+  console.log(playerSelection, computerSelection)
 }
 
 round(getComputerChoise(), getPlayerChoise('ROCk'))
