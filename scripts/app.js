544b7c2220e663d2b4d3ab10e43b0c5ad2dfbd7f
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-26 09:36:20 +0400

    feat: add oppesite side of RPS function

--- scripts/app.js
+++ scripts/app.js
@@ -14,3 +14,35 @@ function getPlayerChoice(choice) {
   return index, choice // [ 0, 'rock' ]
 }
 
+function getOpposite(computerSelection) {
+  const SRP = ['scissors', 'rock', 'paper'] // RPS[0] beats SRP[0] and RPS[1] beats SRP[1]...
+
+  // computerSelection => [ 2, 'scissors' ]
+  let string = SRP[computerSelection[0]] // 'paper'
+  let index = SRP.indexOf(string) // '0'
+
+  return index
+}
+
+
+function getWinner(computerSelection, playerSelection) {
+  let oppositeSide = getOpposite(computerSelection)
+
+  switch (playerSelection[0]) {
+    case computerSelection[0]: // both are equal 
+      console.log('TIE')
+      return -1
+    case oppositeSide:
+      console.log(`You Win! ${playerSelection[1]} beats ${computerSelection[1]}`)
+      return 1
+    default:
+      console.log(`You Lose! ${computerSelection[1]} beats ${playerSelection[1]}`)
+      return 0
+  }
+}
+
+function singleRound(computerSelection, playerSelection) {
+  let winner = getWinner(computerSelection, playerSelection)
+}
+
+singleRound(getComputerChoice(), getPlayerChoice('rock'))
