8039134fd5ec5b1fa0bc7c5318d786412192d607
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-30 17:53:13 +0400

    feat: add game card outline

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -1,10 +1,5 @@
 const gameButtons = document.getElementById('game-buttons')
-const quitBtn = document.getElementById('quit')
 const newGameBtn = document.getElementById('new-game')
-
-quitBtn.addEventListener('click', () => {
-  window.close();
-})
 
 newGameBtn.addEventListener('click', () => {
   gameButtons.style.display = 'none'
