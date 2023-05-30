f47e6ef14d9a3bc68eb6c96982c4a09308ac1bab
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-30 18:30:07 +0400

    feat: update new game button

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -1,7 +1,9 @@
 const gameButtons = document.getElementById('game-buttons')
 const newGameBtn = document.getElementById('new-game')
+const gameCard = document.getElementById('game-card')
 
 newGameBtn.addEventListener('click', () => {
   gameButtons.style.display = 'none'
   gameButtons.previousElementSibling.style.display = 'none'
+  gameCard.style.display = 'flex'
 })
