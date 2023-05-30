42cd7186fa032c0d4700dcdd629e1367504a649a
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-30 17:22:32 +0400

    feat: add choices

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -1,5 +1,12 @@
+const gameButtons = document.getElementById('game-buttons')
 const quitBtn = document.getElementById('quit')
+const newGameBtn = document.getElementById('new-game')
 
 quitBtn.addEventListener('click', () => {
-  window.location.href = "PageUrl";
+  window.close();
+})
+
+newGameBtn.addEventListener('click', () => {
+  gameButtons.style.display = 'none'
+  gameButtons.previousElementSibling.style.display = 'none'
 })
